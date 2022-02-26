8ce9a27fcde2ca9d45e62a7c2d0fa8ab9db6d235
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2022-02-26 22:05:12 +0100

    Zadanie29

--- LAB1/Zad29.py
+++ LAB1/Zad29.py
@@ -1,5 +1,6 @@
 m = [3, -5, 2, 10, -14, 6, 8, 15, 9, 21]
-print(m)
 for i in (m):
     if (i > 0 and 10 >= i):
-        print(i)
+        print("liczby od 0 --> 10 ",i)
+suma=m[0]+m[2]+m[3]+m[5]+m[6]+m[8]
+print(suma)
