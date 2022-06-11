37cfeb8ac8f8b47122e658b85a38c0ee6b50cd3f
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2022-06-11 22:00:15 +0200

    Zadanie1

--- LAB3/Zad1.py
+++ LAB3/Zad1.py
@@ -1,56 +1,37 @@
 import pygad
 import numpy
 
-S = ["zegar", "obraz-pejzaz", "obraz-portret", "radio", "laptop", "lampka-nocna", "srebrne-sztucce", "porcelana",
-     "figura-z-brazu", "skorzana-torebka", "odkurzacz"]
+S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80]
 
-Values = [100, 300, 200, 40, 500, 70, 100, 250, 300, 280, 300]
-Weights = [7, 7, 6, 2, 5, 6, 1, 3, 10, 3, 15]
-
-weightLimit = 25
-
-# definiujemy parametry chromosomu
-# geny to liczby: 0 lub 1
 gene_space = [0, 1]
 
-
-# definiujemy funkcję fitness
 def fitness_func(solution, solution_idx):
-    sumWeight = numpy.sum(solution * Weights)
-    sumValue = numpy.sum(solution * Values)
-    if sumWeight > weightLimit:
-        return 0
-    else:
-        return sumValue
-
+    sum1 = numpy.sum(solution * S)
+    solution_invert = 1 - solution
+    sum2 = numpy.sum(solution_invert * S)
+    fitness = -numpy.abs(sum1-sum2)
+    return fitness
 
 fitness_function = fitness_func
 
-# ile chromsomów w populacji
-# ile genow ma chromosom
 sol_per_pop = 10
 num_genes = len(S)
 
-# ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
-# ile pokolen
-# ilu rodzicow zachowac (kilka procent)
 num_parents_mating = 5
 num_generations = 30
 keep_parents = 2
 
-# jaki typ selekcji rodzicow?
-# sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
+
 parent_selection_type = "sss"
 
-# w il =u punktach robic krzyzowanie?
 crossover_type = "single_point"
 
-# mutacja ma dzialac na ilu procent genow?
-# trzeba pamietac ile genow ma chromosom
-mutation_type = "random"
-mutation_percent_genes = 10
 
-# inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
+
+mutation_type = "random"
+mutation_percent_genes = 8
+
+
 ga_instance = pygad.GA(gene_space=gene_space,
                        num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
@@ -63,24 +44,16 @@ ga_instance = pygad.GA(gene_space=gene_space,
                        mutation_type=mutation_type,
                        mutation_percent_genes=mutation_percent_genes)
 
-# uruchomienie algorytmu
+
 ga_instance.run()
 
-# podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
+
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Parameters of the best solution : {solution}".format(solution=solution))
-
-# przedmioty: nazwy przedmiotów z najlepszych rozwiązań
-print("Item names of the best solution : ")
-for i in range(len(Values)):
-    if solution[i] == 1:
-        print(S[i], end=', ')
-print()
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
-# tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
-prediction = numpy.sum(Values * solution)
+
+prediction = numpy.sum(S*solution)
 print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
 
-# wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
 ga_instance.plot_fitness()
