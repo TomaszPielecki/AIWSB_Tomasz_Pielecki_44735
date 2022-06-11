03ec99baabd101391cf2919c488a73e48edd136e
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2022-06-11 22:28:30 +0200

    Zadanie4

---
+++ LAB3/Zad4.py
@@ -0,0 +1,93 @@
+import pygad
+import numpy
+import time
+
+#15 liczb
+#S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80]
+#25 liczb
+#S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80, 81, 85, 89, 90, 92, 95, 97, 99, 100, 101]
+#35
+#S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80, 81, 85, 89, 90, 92, 95, 97, 99, 100, 101, 103, 105, 107, 110, 111, 113, 117, 118, 120, 121]
+#45 liczb
+S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80, 81, 85, 89, 90, 92, 95, 97, 99, 100, 101, 103, 105, 107, 110, 111, 113, 117, 118, 120, 121, 122, 124, 127, 129, 130, 133, 134, 140, 141, 150]
+
+#definiujemy parametry chromosomu
+#geny to liczby: 0 lub 1
+gene_space = [0, 1]
+
+#definiujemy funkcję fitness
+def fitness_func(solution, solution_idx):
+    sum1 = numpy.sum(solution * S)
+    solution_invert = 1 - solution
+    sum2 = numpy.sum(solution_invert * S)
+    fitness = -numpy.abs(sum1-sum2)
+    #lub: fitness = 1.0 / (1.0 + numpy.abs(sum1-sum2))
+    return fitness
+
+fitness_function = fitness_func
+
+#ile chromsomów w populacji
+#ile genow ma chromosom
+sol_per_pop = 10
+num_genes = len(S)
+
+#ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
+#ile pokolen
+#ilu rodzicow zachowac (kilka procent)
+num_parents_mating = 5
+num_generations = 30
+keep_parents = 2
+
+#jaki typ selekcji rodzicow?
+#sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
+parent_selection_type = "sss"
+
+#w il =u punktach robic krzyzowanie?
+crossover_type = "single_point"
+
+#mutacja ma dzialac na ilu procent genow?
+#trzeba pamietac ile genow ma chromosom
+mutation_type = "random"
+mutation_percent_genes = 5
+tab_time = []
+for i in range(1,11):
+    start = time.time()
+    #inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
+    ga_instance = pygad.GA(gene_space=gene_space,
+                           num_generations=num_generations,
+                           num_parents_mating=num_parents_mating,
+                           fitness_func=fitness_function,
+                           sol_per_pop=sol_per_pop,
+                           num_genes=num_genes,
+                           parent_selection_type=parent_selection_type,
+                           keep_parents=keep_parents,
+                           crossover_type=crossover_type,
+                           mutation_type=mutation_type,
+                           mutation_percent_genes=mutation_percent_genes,
+                           stop_criteria=["reach_0"])
+
+    #uruchomienie algorytmu
+    ga_instance.run()
+    stop = time.time()
+    runtime = stop - start
+    # ile czasu działał algorytm genetyczny
+    print("Algorithm runtime= " + str(runtime))
+    tab_time.append(runtime)
+
+print("Average runtime for 10 runs: " + str(sum(tab_time)/10))
+
+
+#podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Parameters of the best solution : {solution}".format(solution=solution))
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+
+#tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
+prediction = numpy.sum(S*solution)
+print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+
+#ile pokoleń minęło, aż do znalezienia najlepszego rozwiązania.
+print("How many generations passed untill the best solution: {generations_completed}".format(generations_completed=ga_instance.generations_completed))
+ga_instance.generations_completed
+#wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
+ga_instance.plot_fitness()
